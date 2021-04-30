19fef91c594785c8aaff15a40619274ace5d65ba
Dennis Altergott <altergott.dennis1999@gmail.com>
2021-04-30 15:33:50 +0200

    raspy_arduino_connection: changed request_warning.py class file services_and_messages: added message Encoder with 4 int32[] values

--- raspy_arduino_connection/src/request_warning.py
+++ raspy_arduino_connection/src/request_warning.py
@@ -1,27 +1,27 @@
+
+
 class WarningRequest: #defining a class in order to check if the Service received a warning request
 
-    def __init__(self):
-        self.requested_warning = [True, True, True, True]
-        self.got_warning_request = False
-    
-    @staticmethod
-    def requested_warning_msg():
-        return self.requested_warning
-    
-    @staticmethod
-    def received_warning_request():
-        return self.got_warning_request
+    requested_warning = [True, True, True, True]
+    got_warning_request = False
 
-    @staticmethod
-    def set_requested_warning(list_of_true_echo_values):
-        self.requested_warning=list_of_true_echo_values
-    
-    @staticmethod
-    def set_received_warning_request(received_a_request):
-        self.got_warning_request = received_a_request
+    @classmethod
+    def requested_warning_msg(cls):
+        return cls.requested_warning
 
-    @staticmethod
-    def setback_all_values():
-        self.requested_warning = [True, True, True, True]
-        self.got_warning_request = False
-    
+    @classmethod
+    def received_warning_request(cls):
+        return cls.got_warning_request
+
+    @classmethod
+    def set_requested_warning(cls, list_of_true_echo_values):
+        cls.requested_warning=list_of_true_echo_values
+
+    @classmethod
+    def set_received_warning_request(cls, received_a_request):
+        cls.got_warning_request = received_a_request
+
+    @classmethod
+    def setback_all_values(cls):
+        cls.requested_warning = [True, True, True, True]
+        cls.got_warning_request = False
