9d5e038fad6e657f036036e272893690eb1d83f8
tulto <timo.wehner@student.fhws.de>
2021-10-14 23:23:27 +0200

    Adding variable deadzone and speed for movement Joystick

--- odometry/src/movement_joystick.py
+++ odometry/src/movement_joystick.py
@@ -7,14 +7,24 @@ from services_and_messages.msg import Joystick
 from geometry_msgs.msg import Twist
 
 offset = 512
-throttling = 70
+zero = 0
+
+if not rospy.has_param('/movement_joystick/deadzone'):
+    rospy.set_param('/movement_joystick/deadzone', 50)
+
+if not rospy.has_param('/movement_joystick/speed'):
+    rospy.set_param('/movement_joystick/speed', 70)
+
+speed = rospy.get_param('/movement_joystick/speed')
+deadzone = rospy.get_param('/movement_joystick/deadzone')
 
 
 def callback_receive_joystick(msg):
+    global zero
     if msg.button:
         msg_mov = Twist()
         msg_mov.linear.x = (msg.x - offset) / throttling
-        msg_mov.linear.y = (msg.y - offset)/ throttling
+        msg_mov.linear.y = (msg.y - offset) / throttling
         msg_mov.angular.z = (msg.t - offset) / throttling
     if not msg.button:
         msg_mov = Twist()
@@ -22,11 +32,17 @@ def callback_receive_joystick(msg):
         msg_mov.linear.y = (msg.t - offset) / throttling
         msg_mov.angular.z = (msg.y - offset) / throttling
 
-    pub.publish(msg_mov)
+    # only send cmd_vel if joystick is not used
+    if abs(msg.x - offset) > deadzone or abs(msg.y - offset) > deadzone or abs(msg.z - offset) > deadzone or zero == 0:
+        pub.publish(msg_mov)
+        zero = 0
+        # sending once zero to cmd_vel
+        if abs(msg.x - offset) < deadzone and abs(msg.y - offset) < deadzone and abs(msg.z - offset) < deadzone:
+            zero = 1
 
 
 if __name__ == '__main__':
-    rospy.init_node("joystick_movement")
+    rospy.init_node("movement_joystick")
 
     sub = rospy.Subscriber("/movement/joystick", Joystick, callback_receive_joystick)
 
