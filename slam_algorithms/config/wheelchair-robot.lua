a27e1a7d2ee9d21ac02657de886a5971f75d4437
Dennis <dennis.altergott@student.fhws.de>
2021-11-30 16:54:06 +0100

    slam_algorithms: third iteration of tuning parameters for all algorithms (with respect to the ROS WIKI how they affect the computation) (NursingHome as map)

--- slam_algorithms/config/wheelchair-robot.lua
+++ slam_algorithms/config/wheelchair-robot.lua
@@ -17,7 +17,7 @@ options = {
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 5,
   num_point_clouds = 0,
-  lookup_transform_timeout_sec = 1.0,
+  lookup_transform_timeout_sec = 0.5,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 0.05,
   trajectory_publish_period_sec = 0.03,
@@ -34,12 +34,12 @@ TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
 TRAJECTORY_BUILDER_2D.min_range = 0.15
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.0
-TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 30
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 45
+TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 3
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 5
 
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 9.0
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(5.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 1.0
 
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
