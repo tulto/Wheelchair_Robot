bc14532b3a22357bf365a3597cddf67d7bdb3e62
Dennis <dennis.altergott@student.fhws.de>
2021-12-03 10:01:33 +0100

    slam_algorithms: all values for the real robot are now the same like in the first iteration for comparing

--- slam_algorithms/config/wheelchair-robot-real.lua
+++ slam_algorithms/config/wheelchair-robot-real.lua
@@ -17,15 +17,15 @@ options = {
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
-  lookup_transform_timeout_sec = 1.,
+  lookup_transform_timeout_sec = 1.0,
   submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
+  pose_publish_period_sec = 0.05,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
+  rangefinder_sampling_ratio = 1.0,
   odometry_sampling_ratio = 0.5,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
-  landmarks_sampling_ratio = 1.,
+  imu_sampling_ratio = 1.0,
+  landmarks_sampling_ratio = 1.0,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
@@ -39,7 +39,7 @@ TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
 
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
 
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
