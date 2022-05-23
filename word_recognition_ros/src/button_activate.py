169925dd97e4591b5ea0eb0af0e5b926c17f704c
Christian <christianeckerdt@gmail.com>
2022-05-23 16:29:37 +0200

    script for button activation on rasp

---
+++ word_recognition_ros/src/button_activate.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.LOW:
+        print("Button was pushed!")
