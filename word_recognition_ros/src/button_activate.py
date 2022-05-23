4c6fd7ef1999cd9389cdd7a695171f1cdcd6b6e7
Christian <christianeckerdt@gmail.com>
2022-05-23 16:43:33 +0200

    bug fix

--- word_recognition_ros/src/button_activate.py
+++ word_recognition_ros/src/button_activate.py
@@ -3,8 +3,8 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True: # Run forever
-    if GPIO.input(10) == GPIO.LOW:
+    if GPIO.input(17) == GPIO.LOW:
         print("Button was pushed!")
