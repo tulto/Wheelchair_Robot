f7efe0eb46b822108189e259cf93eaec708c24f1
Dennis <dennis.altergott@student.fhws.de>
2022-05-25 15:40:47 +0200

    Added a fix in order to better function when a navigation has yet to be started

--- navigation/src/reset_cmd_vel_on_destination.py
+++ navigation/src/reset_cmd_vel_on_destination.py
@@ -8,28 +8,28 @@ from geometry_msgs.msg import Twist
 
 def callback_is_autonomous_nav_active(msg):
 	global last_status
+	if(len(msg.status_list) != 0):
+		if(last_status == 1 and msg.status_list[0].status == 3):
+			
+			stop_msg.stamp.nsecs = 0
+			stop_msg.stamp.secs = 0
+			stop_msg.id = ""
 
-	if(last_status == 1 and msg.status_list[0].status == 3):
+			for i in range(2):
+				stop_pub.publish(stop_msg)
+				rospy.sleep(0.05)
+			
+			cmd_vel_zero_msg.linear.x = 0
+			cmd_vel_zero_msg.linear.y = 0
+			cmd_vel_zero_msg.linear.z = 0
+			cmd_vel_zero_msg.angular.x = 0
+			cmd_vel_zero_msg.angular.y = 0
+			cmd_vel_zero_msg.angular.z = 0
+			for i in range(10):
+				pub_cmd_vel.publish(cmd_vel_zero_msg)
+				rospy.sleep(0.05)
 		
-		stop_msg.stamp.nsecs = 0
-		stop_msg.stamp.secs = 0
-		stop_msg.id = ""
-
-		for i in range(2):
-			stop_pub.publish(stop_msg)
-			rospy.sleep(0.05)
-		
-		cmd_vel_zero_msg.linear.x = 0
-		cmd_vel_zero_msg.linear.y = 0
-		cmd_vel_zero_msg.linear.z = 0
-		cmd_vel_zero_msg.angular.x = 0
-		cmd_vel_zero_msg.angular.y = 0
-		cmd_vel_zero_msg.angular.z = 0
-		for i in range(10):
-			pub_cmd_vel.publish(cmd_vel_zero_msg)
-			rospy.sleep(0.05)
-	
-	last_status = msg.status_list[0].status #give status to last_status
+		last_status = msg.status_list[0].status #give status to last_status
 
 
 if __name__ == '__main__':
