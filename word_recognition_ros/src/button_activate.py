dc76f4dd2b6fe6cf5d388cf715ec095d30913da5
Christian <christianeckerdt@gmail.com>
2022-08-17 16:18:40 +0200

    Fixing issues with microphone file
    added buttonactivate.py from gui branch because of merging errors

--- word_recognition_ros/src/button_activate.py
+++ word_recognition_ros/src/button_activate.py
@@ -18,17 +18,16 @@ def ZScore(data):
     data_ZScore = np.array((data - Mean) / Std)
     return data_ZScore
 
-def recording(sample_rate=22050,duration= 6,rec_duration=1.5,sec_slide=0.1,word_threshold=0.5,number_index= 10):
-    
-    print("Starting recording..")
-    record = rec(int(duration*sample_rate),samplerate=sample_rate, channels=1, blocking=False)
-    wait()
-    print("Recording finished..")
+def butter_lowpass(cutoff, fs, order=5):
+    return butter(order, cutoff, fs=fs, btype='low', analog=False)
 
-    from librosa.feature import mfcc
-    from tflite_runtime.interpreter import Interpreter
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
-    record = np.squeeze(record)
+def callback_subscriber_active(msg_active):
+    global button_was_pushed
 
     if msg_active.status_list[0].status == 1:
         button_was_pushed = False
@@ -48,62 +47,54 @@ def recording(sample_rate=22050,duration= 6,rec_duration=1.5,sec_slide=0.1,word_
             wait()
             print("Recording finished..")
 
-    model_path = '4_layer_globalpool.tflite'
+            from librosa.feature import mfcc
+            from tflite_runtime.interpreter import Interpreter
+
+            record = np.squeeze(record)
+
+            record = butter_lowpass_filter(data=record,cutoff=10000, fs=sample_rate)
+
+            model_path = '4_layer_globalpool.tflite'
 
 
-    # Load model (interpreter)
-    interpreter = Interpreter(model_path)
-    interpreter.allocate_tensors()
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+            # Load model (interpreter)
+            interpreter = Interpreter(model_path)
+            interpreter.allocate_tensors()
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
 
 
-    steps_to_make=int((duration-rec_duration)/sec_slide)
+            steps_to_make=int((duration-rec_duration)/sec_slide)
 
 
 
-    for i in range(1, steps_to_make+1):
-        
-        if i==1:
-            window = record[:int(sample_rate * rec_duration)]
+            for i in range(1, steps_to_make+1):
+                if i==1:
+                    window = record[:int(sample_rate * rec_duration)]
 
-        else:
-            window=record[int(i*sec_slide*sample_rate):int((sample_rate*(rec_duration+i*sec_slide)))]
+                else:
+                    window=record[int(i*sec_slide*sample_rate):int((sample_rate*(rec_duration+i*sec_slide)))]
 
-        window_mfcc=mfcc(y=window, sr=sample_rate, n_mfcc=20, n_fft=512)
+                window_mfcc=mfcc(y=window, sr=sample_rate, n_mfcc=20, n_fft=512)
 
-        window_mfcc_z=ZScore(window_mfcc)
+                window_mfcc_z=ZScore(window_mfcc)
 
-        window_mfcc_z = np.pad(window_mfcc_z, pad_width=((0, 0), (0, 1)), mode='constant')
+                window_mfcc_z = np.pad(window_mfcc_z, pad_width=((0, 0), (0, 1)), mode='constant')
 
 
-        in_tensor = np.float32(window_mfcc_z.reshape(1, (window_mfcc_z.shape[0]), window_mfcc_z.shape[1], 1))
+                in_tensor = np.float32(window_mfcc_z.reshape(1, (window_mfcc_z.shape[0]), window_mfcc_z.shape[1], 1))
 
-        interpreter.set_tensor(input_details[0]['index'], in_tensor)
-        interpreter.invoke()
-        output_data = interpreter.get_tensor(output_details[0]['index'])
+                interpreter.set_tensor(input_details[0]['index'], in_tensor)
+                interpreter.invoke()
+                output_data = interpreter.get_tensor(output_details[0]['index'])
 
-        output_data= np.around(output_data[0], 2)
+                output_data= np.around(output_data[0], 2)
 
-        if np.amax(output_data) > word_threshold:
-            ##
+                if np.amax(output_data) > word_threshold:
+                    global number_index
+                    number_index = np.argmax(output_data)
 
-            number_index = np.argmax(output_data)
-
-        
-
-    if number_index < 10: 
-        list =["Aufenthaltsraum", "Cafe", "Gruppenraum", "Ruheraum", "Schlafzimmer", "Speisesaal"]
-        #print(list[number_index])
-        msg = String()
-        msg.data = list[number_index]
-        goal_pub.publish(msg)
-        number_index = 10 
-    else: 
-        pass
-
-def butter_lowpass(cutoff, fs, order=5):
-    return butter(order, cutoff, fs=fs, btype='low', analog=False)
+                
 
             if number_index < 10: 
                 list =["Aufenthaltsraum", "Cafe", "Gruppenraum", "Ruheraum", "Schlafzimmer", "Speisesaal"]
@@ -119,13 +110,6 @@ def butter_lowpass(cutoff, fs, order=5):
                 
         button_was_pushed = False
 
-def callback_subscriber_active(msg_active):
-    global button_was_pushed
-    
-    if len(msg_active.status_list) == 0:
-        if (button_was_pushed):
-            recording()
-        
 
     mic_msg.data = False  
     rospy.sleep(1)
@@ -143,6 +127,19 @@ if __name__ == '__main__':
 
     global button_was_pushed
     button_was_pushed = False
+    global sample_rate
+    sample_rate=22050
+    global duration
+    duration= 6
+
+    global rec_duration
+    rec_duration=1.5
+    global sec_slide
+    sec_slide=0.1
+    global word_threshold
+    word_threshold=0.5
+    global number_index 
+    number_index= 10
      
     
     
