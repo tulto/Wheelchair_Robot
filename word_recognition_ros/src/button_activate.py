183043bfa2ae75bb5a825147817d15b982c340ba
Christian <christianeckerdt@gmail.com>
2022-08-17 17:19:15 +0200

    bug fix button

--- word_recognition_ros/src/button_activate.py
+++ word_recognition_ros/src/button_activate.py
@@ -29,86 +29,91 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
 def callback_subscriber_active(msg_active):
     global button_was_pushed
 
-    if msg_active.status_list[0].status == 1:
-        button_was_pushed = False
-        print("Recognition is not possible at this moment. Navigation is active!!!")
+    if len(msg_active.status_list[0].status)== 0:
+        pass
+
     else:
-        if (button_was_pushed):
-            print("Starting recording..")
 
-            #For gui: microphone activ 
-            mic_msg= Bool()
-            mic_msg.data=True
+        if msg_active.status_list[0].status == 1:
+            button_was_pushed = False
+            print("Recognition is not possible at this moment. Navigation is active!!!")
+        else:
+            if (button_was_pushed):
+                print("Starting recording..")
 
-            mic_pub.publish(mic_msg)
+                #For gui: microphone activ 
+                mic_msg= Bool()
+                mic_msg.data=True
 
-
-            record = rec(int(duration*sample_rate),samplerate=sample_rate, channels=1, blocking=False)
-            wait()
-            print("Recording finished..")
-
-            from librosa.feature import mfcc
-            from tflite_runtime.interpreter import Interpreter
-
-            record = np.squeeze(record)
-
-            record = butter_lowpass_filter(data=record,cutoff=10000, fs=sample_rate)
-
-            model_path = '4_layer_globalpool.tflite'
+                mic_pub.publish(mic_msg)
 
 
-            # Load model (interpreter)
-            interpreter = Interpreter(model_path)
-            interpreter.allocate_tensors()
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
+                record = rec(int(duration*sample_rate),samplerate=sample_rate, channels=1, blocking=False)
+                wait()
+                print("Recording finished..")
+
+                from librosa.feature import mfcc
+                from tflite_runtime.interpreter import Interpreter
+
+                record = np.squeeze(record)
+
+                record = butter_lowpass_filter(data=record,cutoff=10000, fs=sample_rate)
+
+                model_path = '4_layer_globalpool.tflite'
 
 
-            steps_to_make=int((duration-rec_duration)/sec_slide)
+                # Load model (interpreter)
+                interpreter = Interpreter(model_path)
+                interpreter.allocate_tensors()
+                input_details = interpreter.get_input_details()
+                output_details = interpreter.get_output_details()
+
+
+                steps_to_make=int((duration-rec_duration)/sec_slide)
 
 
 
-            for i in range(1, steps_to_make+1):
-                if i==1:
-                    window = record[:int(sample_rate * rec_duration)]
+                for i in range(1, steps_to_make+1):
+                    if i==1:
+                        window = record[:int(sample_rate * rec_duration)]
 
-                else:
-                    window=record[int(i*sec_slide*sample_rate):int((sample_rate*(rec_duration+i*sec_slide)))]
+                    else:
+                        window=record[int(i*sec_slide*sample_rate):int((sample_rate*(rec_duration+i*sec_slide)))]
 
-                window_mfcc=mfcc(y=window, sr=sample_rate, n_mfcc=20, n_fft=512)
+                    window_mfcc=mfcc(y=window, sr=sample_rate, n_mfcc=20, n_fft=512)
 
-                window_mfcc_z=ZScore(window_mfcc)
+                    window_mfcc_z=ZScore(window_mfcc)
 
-                window_mfcc_z = np.pad(window_mfcc_z, pad_width=((0, 0), (0, 1)), mode='constant')
+                    window_mfcc_z = np.pad(window_mfcc_z, pad_width=((0, 0), (0, 1)), mode='constant')
 
 
-                in_tensor = np.float32(window_mfcc_z.reshape(1, (window_mfcc_z.shape[0]), window_mfcc_z.shape[1], 1))
+                    in_tensor = np.float32(window_mfcc_z.reshape(1, (window_mfcc_z.shape[0]), window_mfcc_z.shape[1], 1))
 
-                interpreter.set_tensor(input_details[0]['index'], in_tensor)
-                interpreter.invoke()
-                output_data = interpreter.get_tensor(output_details[0]['index'])
+                    interpreter.set_tensor(input_details[0]['index'], in_tensor)
+                    interpreter.invoke()
+                    output_data = interpreter.get_tensor(output_details[0]['index'])
 
-                output_data= np.around(output_data[0], 2)
+                    output_data= np.around(output_data[0], 2)
 
-                if np.amax(output_data) > word_threshold:
-                    global number_index
-                    number_index = np.argmax(output_data)
+                    if np.amax(output_data) > word_threshold:
+                        global number_index
+                        number_index = np.argmax(output_data)
 
-                
+                    
 
-            if number_index < 10: 
-                list =["Aufenthaltsraum", "Cafe", "Gruppenraum", "Ruheraum", "Schlafzimmer", "Speisesaal"]
-                #print(list[number_index])
-                msg = String()
-                msg.data = list[number_index]
-                goal_pub.publish(msg)
-                number_index = 10 
-            else: 
-                pass
-        
-        
-                
-        button_was_pushed = False
+                if number_index < 10: 
+                    list =["Aufenthaltsraum", "Cafe", "Gruppenraum", "Ruheraum", "Schlafzimmer", "Speisesaal"]
+                    #print(list[number_index])
+                    msg = String()
+                    msg.data = list[number_index]
+                    goal_pub.publish(msg)
+                    number_index = 10 
+                else: 
+                    pass
+            
+            
+                    
+            button_was_pushed = False
 
 
     mic_msg.data = False  
